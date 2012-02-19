5d074f6453381ef87688381e7a3ad9747fa147ea
Simon Whitaker <simon@goosoftware.co.uk>
2012-02-19 21:51:32 +0000

    Adding link to info for beta testers

--- Zippity/GSRootListViewController.m
+++ Zippity/GSRootListViewController.m
@@ -41,6 +41,62 @@
     [self.tableView reloadData];
 }
 
+#pragma mark - Table view data source methods
+
+- (NSInteger)numberOfSectionsInTableView:(UITableView *)tableView
+{
+    // Return the number of sections.
+    return 2;
+}
+
+- (NSInteger)tableView:(UITableView *)tableView numberOfRowsInSection:(NSInteger)section
+{
+    if (section == 1) {
+        return 1;
+    }
+    return [super tableView:tableView numberOfRowsInSection:section];
+}
+
+- (UITableViewCell*)tableView:(UITableView *)tableView cellForRowAtIndexPath:(NSIndexPath *)indexPath
+{
+    UITableViewCell *cell;
+    if (indexPath.section == 1) {
+        static NSString *CellIdentifier = @"DevNotes";
+        
+        cell = [tableView dequeueReusableCellWithIdentifier:CellIdentifier];
+        if (cell == nil) {
+            cell = [[UITableViewCell alloc] initWithStyle:UITableViewCellStyleDefault reuseIdentifier:CellIdentifier];
+        }
+        
+        cell.textLabel.text = @"Info for beta testers";
+        cell.imageView.image = [UIImage imageNamed:@"safari-icon.png"];
+    } else {
+        cell = [super tableView:tableView cellForRowAtIndexPath:indexPath];
+        
+        GSFileSystemEntity *fse = [self.container.contents objectAtIndex:indexPath.row];
+        GSAppDelegate *appDelegate = (GSAppDelegate*)[[UIApplication sharedApplication] delegate];
+        if (self == [appDelegate.navigationController.viewControllers objectAtIndex:0] && !fse.isVisited) {
+            cell.accessoryView = [[UIImageView alloc] initWithImage:[UIImage imageNamed:@"new-file-marker.png"]];
+        } else {
+            cell.accessoryView = nil;
+        }
+    }
+    
+    return cell;
+}
+
+#pragma mark - Table view delegate methods
+
+- (void)tableView:(UITableView *)tableView didSelectRowAtIndexPath:(NSIndexPath *)indexPath
+{
+    if (indexPath.section == 1) {
+        [tableView deselectRowAtIndexPath:indexPath animated:NO];
+        [[UIApplication sharedApplication] openURL:[NSURL URLWithString:@"http://dl.dropbox.com/u/363683/zippity-testers.md"]];
+    } else {
+        [super tableView:tableView didSelectRowAtIndexPath:indexPath];
+    }
+}
+
 - (BOOL)tableView:(UITableView *)tableView canEditRowAtIndexPath:(NSIndexPath *)indexPath
 {
     // Return NO if you do not want the specified item to be editable.
@@ -61,21 +117,6 @@
         
         [tableView deleteRowsAtIndexPaths:[NSArray arrayWithObject:indexPath] withRowAnimation:UITableViewRowAnimationFade];
     }   
-}
-
-- (UITableViewCell*)tableView:(UITableView *)tableView cellForRowAtIndexPath:(NSIndexPath *)indexPath
-{
-    UITableViewCell *cell = [super tableView:tableView cellForRowAtIndexPath:indexPath];
-    
-    GSFileSystemEntity *fse = [self.container.contents objectAtIndex:indexPath.row];
-    GSAppDelegate *appDelegate = (GSAppDelegate*)[[UIApplication sharedApplication] delegate];
-    if (self == [appDelegate.navigationController.viewControllers objectAtIndex:0] && !fse.isVisited) {
-        cell.accessoryView = [[UIImageView alloc] initWithImage:[UIImage imageNamed:@"new-file-marker.png"]];
-    } else {
-        cell.accessoryView = nil;
-    }
-    
-    return cell;
 }
 
 @end
