821cfad4798cb866736e9400edf4aa024d6a4969
Simon Whitaker <simon@goosoftware.co.uk>
2012-02-22 04:49:52 +0000

    Only allow editing in the zip file section

--- Zippity/GSRootListViewController.m
+++ Zippity/GSRootListViewController.m
@@ -91,7 +91,7 @@
 - (BOOL)tableView:(UITableView *)tableView canEditRowAtIndexPath:(NSIndexPath *)indexPath
 {
     // Return NO if you do not want the specified item to be editable.
-    return YES;
+    return indexPath.section == 0;
 }
 
 - (void)tableView:(UITableView *)tableView commitEditingStyle:(UITableViewCellEditingStyle)editingStyle forRowAtIndexPath:(NSIndexPath *)indexPath
